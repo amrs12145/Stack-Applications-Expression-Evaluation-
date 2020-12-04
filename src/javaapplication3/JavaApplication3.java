5ee22487594251c3c92835d3dc17ccb2d6d30c28
Amr Samy <amrs12145@gmail.com>
2020-12-04 23:38:48 +0200

    first

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,14 @@
+
+package javaapplication3;
+
+
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
